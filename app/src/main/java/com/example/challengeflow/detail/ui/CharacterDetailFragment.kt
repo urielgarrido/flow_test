ddc06f7fd19ffa4125749473ee7ce2bbb3e84a6c
uriel <urielg@cryptouno.app>
2022-09-02 11:13:57 -0300

    Se agrega un toast de error en la obtencion del primer character.

--- app/src/main/java/com/example/challengeflow/detail/ui/CharacterDetailFragment.kt
+++ app/src/main/java/com/example/challengeflow/detail/ui/CharacterDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -67,9 +68,15 @@ class CharacterDetailFragment : Fragment() {
                 character?.let {
                     characterDetail = it
                     updateView()
+                } ?: run {
+                    showErrorToast()
                 }
             }
         }
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(context, getString(R.string.error_detail), Toast.LENGTH_SHORT).show()
     }
 
     private fun updateView() {
